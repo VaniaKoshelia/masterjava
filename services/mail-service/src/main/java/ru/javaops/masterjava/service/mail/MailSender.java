eec7b067002bc84b87ec510b867b5ff481a78b63
ikoshelia <vanya.koshelya.99@ukr.net>
2019-03-07 01:09:00 +0200

    lesson 6

---
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
@@ -0,0 +1,17 @@
+package ru.javaops.masterjava.service.mail;
+
+import com.typesafe.config.Config;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
+import ru.javaops.masterjava.config.Configs;
+
+import java.util.List;
+
+@Slf4j
+public class MailSender {
+    static void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) {
+        log.info("Send mail to \'" + to + "\' cc \'" + cc + "\' subject \'" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
+    }
+}
